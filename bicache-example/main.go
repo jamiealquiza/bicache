def9f7f1f9acd416adce9e529e98cde7f6ec527f
Jamie Alquiza <jamiealquiza@gmail.com>
2016-11-07 16:47:21 -0700

    timers on bicache-example

--- bicache-example/main.go
+++ bicache-example/main.go
@@ -3,14 +3,16 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"time"
 
+	"github.com/jamiealquiza/tachymeter"
 	"github.com/jamiealquiza/bicache"
 )
 
 func main() {
 	c := bicache.New(&bicache.Config{
-		MfuSize: 10,
-		MruSize: 3,
+		MfuSize: 100,
+		MruSize: 100000,
 	})
 
 	c.Set("one", "one")
@@ -22,10 +24,34 @@ func main() {
 
 	fmt.Println(c.Get("one"))
 
+	fmt.Println()
+
 	stats := c.Stats()
 	j, _ := json.Marshal(stats)
 	fmt.Println(string(j))
 
 	c.Delete("one")
-	fmt.Println(c.Get("one"))
+
+	t := tachymeter.New(&tachymeter.Config{Size: 100000})
+	fmt.Println("[ Set 100000 keys ]")
+	for i := 0; i < 100000; i++ {
+		start := time.Now()
+		c.Set(i, i*2)
+		t.AddTime(time.Since(start))
+	}
+	t.AddCount(10000)
+	t.Calc().Dump()
+
+	fmt.Println()
+
+	t.Reset()
+	fmt.Println("[ Get 100000 keys ]")
+	for i := 0; i < 100000; i++ {
+		start := time.Now()
+		_ = c.Get(i)
+		t.AddTime(time.Since(start))
+	}
+	t.AddCount(10000)
+
+	t.Calc().Dump()
 }
