25b5c168b01ed666f25caa319c173c57a699426d
Jamie Alquiza <jamiealquiza@gmail.com>
2017-03-31 13:37:47 -0600

    added general bicache tests

--- bicache_test.go
+++ bicache_test.go
@@ -1,51 +1,174 @@
-package bicache
+package bicache_test
 
 import (
+	"log"
 	"strconv"
 	"testing"
+	"time"
+
+	"github.com/jamiealquiza/bicache"
 )
 
-func BenchmarkGet(b *testing.B) {
-	b.StopTimer()
+func TestNew(t *testing.T) {
+	// MFU size, MRU size, shard count, expected bicache size.
+	configExpected := [][4]int{
+		[4]int{0, 1, 512, 512},
+		[4]int{1, 2, 512, 1024},
+		[4]int{256, 1024, 512, 1536},
+		[4]int{10000, 30000, 512, 40448}}
 
-	c := New(&Config{
-		MfuSize:    10000,
-		MruSize:    600000,
-		ShardCount: 1024,
-		AutoEvict:  30000,
-	})
+	// Check that the cache sizes are fitted to the
+	// minimum for the number of shards specified.
+	for _, n := range configExpected {
+		c, _ := bicache.New(&bicache.Config{
+			MfuSize:    uint(n[0]),
+			MruSize:    uint(n[1]),
+			ShardCount: n[2],
+		})
 
-	keys := make([]string, b.N)
-	for i := 0; i < b.N; i++ {
-		k := strconv.Itoa(i)
-		keys[i] = k
-		c.Set(k, "my value")
-	}
-
-	b.StartTimer()
-	for i := 0; i < b.N; i++ {
-		c.Get(keys[i])
+		if c.Size != n[3] {
+			t.Errorf("Expected bicache size %d, got %d", n[3], c.Size)
+		}
 	}
 }
 
-func BenchmarkSet(b *testing.B) {
-	b.StopTimer()
-
-	c := New(&Config{
-		MfuSize:    10000,
-		MruSize:    600000,
-		ShardCount: 1024,
-		AutoEvict:  30000,
+func TestStats(t *testing.T) {
+	c, _ := bicache.New(&bicache.Config{
+		MfuSize:    10,
+		MruSize:    30,
+		ShardCount: 2,
+		AutoEvict:  3000,
 	})
 
-	keys := make([]string, b.N)
-	for i := 0; i < b.N; i++ {
-		k := strconv.Itoa(i)
-		keys[i] = k
+	for i := 0; i < 50; i++ {
+		c.Set(strconv.Itoa(i), "value")
+		c.Get(strconv.Itoa(i))
+		c.Get(strconv.Itoa(i))
 	}
 
-	b.StartTimer()
-	for i := 0; i < b.N; i++ {
-		c.Set(keys[i], "my value")
+	c.Get("nil")
+
+	log.Printf("Sleeping for 4 seconds to allow evictions")
+	time.Sleep(4 * time.Second)
+
+	stats := c.Stats()
+
+	if stats.MfuSize != 10 {
+		t.Errorf("Expected MFU size 10, got %d", stats.MfuSize)
+	}
+
+	if stats.MruSize != 30 {
+		t.Errorf("Expected MRU size 30, got %d", stats.MruSize)
+	}
+
+	if stats.MfuUsedP != 100 {
+		t.Errorf("Expected MFU usedp 100, got %d", stats.MfuUsedP)
+	}
+
+	if stats.MruUsedP != 100 {
+		t.Errorf("Expected MRU usedp 100, got %d", stats.MruUsedP)
+	}
+
+	if stats.Hits != 100 {
+		t.Errorf("Expected 100 hits, got %d", stats.Hits)
+	}
+
+	if stats.Misses != 1 {
+		t.Errorf("Expected 1 misses, got %d", stats.Misses)
+	}
+
+	if stats.Evictions != 10 {
+		t.Errorf("Expected 10 evictions, got %d", stats.Evictions)
+	}
+
+	if stats.Overflows != 0 {
+		t.Errorf("Expected 0 overflows, got %d", stats.Overflows)
+	}
+}
+
+func TestEvictTtl(t *testing.T) {
+	c, _ := bicache.New(&bicache.Config{
+		MfuSize:    10,
+		MruSize:    30,
+		ShardCount: 2,
+		AutoEvict:  1000,
+	})
+
+	c.SetTtl("5", "value", 5)
+	c.SetTtl("30", "value", 30)
+
+	if v := c.Get("5"); v != "value" {
+		t.Error("Expected hit")
+	}
+
+	log.Printf("Sleeping for 6 seconds to allow evictions")
+	time.Sleep(6 * time.Second)
+
+	// Check that the value was evicted.
+	if v := c.Get("5"); v != nil {
+		t.Error("Expected miss")
+	}
+
+	stats := c.Stats()
+
+	if stats.MruSize != 1 || stats.Evictions != 1 {
+		t.Error("Unexpected stats")
+	}
+}
+
+func TestPromoteEvict(t *testing.T) {
+	// Also covers MRU tail eviction.
+	c, _ := bicache.New(&bicache.Config{
+		MfuSize:    10,
+		MruSize:    30,
+		ShardCount: 2,
+		AutoEvict:  5000,
+	})
+
+	for i := 0; i < 50; i++ {
+		c.Set(strconv.Itoa(i), "value")
+	}
+
+	c.Get("0")
+	c.Get("0")
+	c.Get("0")
+	c.Get("0")
+
+	c.Get("1")
+	c.Get("1")
+	c.Get("1")
+
+	c.Get("2")
+	c.Get("2")
+	c.Get("2")
+	c.Get("2")
+	c.Get("2")
+	c.Get("2")
+
+	log.Printf("Sleeping for 6 seconds to allow evictions")
+	time.Sleep(6 * time.Second)
+
+	stats := c.Stats()
+
+	if stats.MfuSize != 3 {
+		t.Error("Unexpected MFU count")
+	}
+
+	if stats.MruSize != 30 {
+		t.Error("Unexpected MRU count")
+	}
+
+	// Check that the expected MRU members exist.
+	for k := 20; k <= 49; k++ {
+		if v := c.Get(strconv.Itoa(k)); v == nil {
+			t.Errorf("Unexpected miss for key %d", k)
+		}
+	}
+
+	// Check that the expected MFU members exist.
+	for _, k := range []int{0, 1, 2} {
+		if v := c.Get(strconv.Itoa(k)); v == nil {
+			t.Errorf("Unexpected miss for key %d", k)
+		}
 	}
 }
